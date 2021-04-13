2c98a7cd8b2fe53484ddf9f803ba92b631726e1e
Generoso Carbone <generoso.carbone@gmail.com>
2021-04-13 18:58:43 +0200

    analisi dati ato2

--- extract_stasis.py
+++ extract_stasis.py
@@ -5,32 +5,7 @@ import matplotlib.pyplot as plt
 import math
 
 
-G = 9.81
-ACCELERATION_IMPACT_COEFFICIENT = 2.5
-ACCELERATION_FREE_FALL_COEFFICIENT = 0.3
-ACCELERATION_IMPACT = ACCELERATION_IMPACT_COEFFICIENT * G
-ACCELERATION_FREE_FALL = ACCELERATION_FREE_FALL_COEFFICIENT * G
-
-MIN_SQM = 1.0
-
-MAX_SPIKES = 2
-
-MEDIANA_INTERVAL_SIZE = 17
-LOWER_BOUND_MEDIANA = 0.89
-UPPER_BOUND_MEDIANA = 1.13
-
-acceleration_impact_analyzed = 0
-
-print('_G: {}'.format(G))
-print('_ACCELERATION_IMPACT: {} ({})'.format(ACCELERATION_IMPACT, ACCELERATION_IMPACT_COEFFICIENT))
-print('_ACCELERATION_FREE_FALL: {} ({})'.format(ACCELERATION_FREE_FALL, ACCELERATION_FREE_FALL_COEFFICIENT))
-print('_MIN_SQM: {}'.format(MIN_SQM))
-print('_MAX_SPIKES: {}'.format(MAX_SPIKES))
-print('_MEDIANA_INTERVAL_SIZE: {}'.format(MEDIANA_INTERVAL_SIZE))
-print('_LOWER_BOUND_MEDIANA: {}'.format(LOWER_BOUND_MEDIANA))
-print('_UPPER_BOUND_MEDIANA: {}'.format(UPPER_BOUND_MEDIANA))
-
-def extract_stasis(df, csv_name, csv_path):
+def extract_stasis(df, csv_name, csv_path, ACCELERATION_IMPACT):
     global acceleration_impact_analyzed
     print('\n__dataframe: {}__'.format(csv_name))
     # df.plot.line()
@@ -40,6 +15,7 @@ def extract_stasis(df, csv_name, csv_path):
 
     last = 0
     count = 1
+    acceleration_impact_analyzed = 0
     for j in range(spike.index.size):
         i = spike.index[j]
         if last == 0:
@@ -74,9 +50,10 @@ def set_parameters(csv_path, csv_name, G, ACCELERATION_IMPACT_COEFFICIENT, ACCEL
     print('_LOWER_BOUND_MEDIANA: {}'.format(LOWER_BOUND_MEDIANA))
     print('_UPPER_BOUND_MEDIANA: {}'.format(UPPER_BOUND_MEDIANA))
     stasi = pandas.read_csv('{}/{}.csv'.format(csv_path, csv_name), sep=';')
-    extract_stasis(stasi, csv_name, csv_path)
+    extract_stasis(stasi, csv_name, csv_path, ACCELERATION_IMPACT)
 
 
-csv_path='shock-2/TP'
-csv_name='fall_1616171234781'
-set_parameters(csv_path, csv_name, 9.81, 2.5, 0.3, 1.0, 2, 17, 0.89, 1.13)
+csv_path='ato-2/fall_20210406'
+csv_name='fall_20210406'
+ACC = 2.5
+set_parameters(csv_path, csv_name, 9.81, ACC, 0.3, 1.0, 2, 17, 0.89, 1.13)
